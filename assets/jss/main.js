992db6e9b7bc61a836df8cb5085e71de362b2baf
sand10man <31373893+sand10man@users.noreply.github.com>
2018-02-06 19:33:22 -0800

    my first commit

---
+++ assets/jss/main.js
@@ -0,0 +1,11 @@
+// not working in either version
+function yearFunction() {
+    var d = new Date();
+    var n = d.getFullYear();
+    document.getElementById("year").innerHTML = n;
+} ()
+
+
+
+
+// document.getElementById('year').innerHTML= new Date().getFullYear
