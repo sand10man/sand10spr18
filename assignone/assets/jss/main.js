36b4b02785d36733a58d593ef3cf10ae79deca0e
sand10man <31373893+sand10man@users.noreply.github.com>
2018-02-09 14:26:53 -0800

    commit again

---
+++ assignone/assets/jss/main.js
@@ -0,0 +1,11 @@
+// current year is displayed
+(function yearFunction() {
+    let d = new Date();
+    let n = d.getFullYear();
+    document.getElementById("year").innerHTML = n;
+})()
+
+
+
+
+// document.getElementById('year').innerHTML= new Date().getFullYear
